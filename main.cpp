3519fb3db5a8dcb2e61ae042efb699df1ad1f2cb
Shadow-nx <32244138+Shadow-nx@users.noreply.github.com>
2018-11-08 06:18:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
-#include "googletest-bc2d0935b74917be0821bfd834472ed9cc4a3b5b/googletest/include/gtest/gtest.h"
+#include <iostream>
 #include "json.hpp"
 
+int main(){
+
+const char * const json_data = R"({
+    "lastname" : "Ivanov",
+    "firstname" : "Ivan",
+    "age" : 25,
+    "islegal" : false,
+    "marks" : [
+    	4,5,5,5,2,3
+    ],
+    "address" : {
+    	"city" : "Moscow",
+        "street" : "Vozdvijenka"
+    }
+})";
+
+    Json object = Json::parse(json_data);
+    cout<<std::any_cast<std::string>(object["lastname"])<<endl;
+    cout<<std::any_cast<bool>(object["islegal"])<<endl;
+    cout<<std::any_cast<int>(object["age"])<<endl;
+
+    auto marks = std::any_cast<Json>(object["marks"]);
+    cout<<std::any_cast<int>(marks[0])<<endl;
+    cout<<std::any_cast<int>(marks[1])<<endl;
+
+    auto address = std::any_cast<Json>(object["address"]);
+    cout<<std::any_cast<std::string>(address["city"])<<endl;
+    cout<<std::any_cast<std::string>(address["street"])<<endl;
+
+return 0;
+}
+/*
 namespace {
 
 const char * const json_data = R"(
@@ -32,3 +64,4 @@ TEST(Json, LoadFromString) {
   EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
   EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
 }
+*/
